194446bef89f3f14b23ac8edf829f3e4977a5d2f
Adam <madaoikonip@gmail.com>
2024-01-14 21:30:46 +0100

    working project

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    dom = 'Hello World'
+    context = {
+        'dom' : dom,
+    }
+    return render(request, 'home.html', context)
--- testDeployDjango/urls.py
+++ testDeployDjango/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from mainApp import views as mainAppView
 
 urlpatterns = [
     path('', mainAppView.home),
